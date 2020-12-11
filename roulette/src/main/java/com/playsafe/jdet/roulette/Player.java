bcd5920b3f5c58bf359a6d8d987371a0a268b482
Fact S Musingarimi <fmusingarimi@gmail.com>
2020-12-11 05:40:40 +0200

    Created a Player class

---
+++ roulette/src/main/java/com/playsafe/jdet/roulette/Player.java
@@ -0,0 +1,49 @@
+package com.playsafe.jdet.roulette;
+
+/**
+ * @author Fact S Musingarimi
+ * 11/12/2020
+ * 05:38
+ */
+public class Player {
+    private final String name;
+    private double totalWinnings;
+    private double totalBets;
+
+    private Player(String name) {
+        this.name = name;
+    }
+
+    public static Player of(String name) {
+        return new Player(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTotalWinnings() {
+        return totalWinnings;
+    }
+
+    public void setTotalWinnings(double totalWinnings) {
+        this.totalWinnings = totalWinnings;
+    }
+
+    public double getTotalBets() {
+        return totalBets;
+    }
+
+    public void setTotalBets(double totalBets) {
+        this.totalBets = totalBets;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", totalWinnings=" + totalWinnings +
+                ", totalBets=" + totalBets +
+                '}';
+    }
+}
