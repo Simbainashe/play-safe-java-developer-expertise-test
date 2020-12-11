fa037fbe2a4979f8d4f4b7f2696f91f553fa9bb1
Fact S Musingarimi <fmusingarimi@gmail.com>
2020-12-11 06:19:19 +0200

    Define winnings calculator
    WinningsCalculator is an interface that should be implemented for each possible bet option

---
+++ roulette/src/main/java/com/playsafe/jdet/roulette/WinningsCalculator.java
@@ -0,0 +1,10 @@
+package com.playsafe.jdet.roulette;
+
+/**
+ * @author Fact S Musingarimi
+ * 11/12/2020
+ * 06:01
+ */
+public interface WinningsCalculator {
+    double calculateWinnings(RouletteWheel rouletteWheel, Bet bet);
+}
